7b0561938b8fab7106951bec55ffcf65c16012b9
cocochi-mt <33363125+LycheeCocoa@users.noreply.github.com>
2021-11-16 15:48:25 -0600

    added model predictions

--- app.py
+++ app.py
@@ -10,8 +10,21 @@ def create_app():
     def root():
         return render_template('index.html', title="Home")
 
-    @app.route('/predict/')
+    @app.route('/predict/', methods=['GET', 'POST'])
     def predict():
-        return predict_kickstart()
+        # 0 Goal
+        # 1 Duration
+        # 2 - 15 Cats
+        # 16 - 19 Countries
+        goal = request.form.get('goal')
+        duration = request.form.get('duration')
+        cat = request.form.get('category')
+        country = request.form.get('country')
+
+        prediction = predict_kickstart(goal, duration, cat, country)
+        message = 'Your Kickstarter is {}% likely to succeed!'.format(
+            (prediction[0][1] * 100).round(2))
+
+        return render_template('prediction.html', message=message)
 
     return app
--- predict.py
+++ predict.py
@@ -1,6 +1,14 @@
 from xgboost import XGBClassifier
+import numpy as np
+
 model = XGBClassifier()
 model.load_model('test_model')
 
-def predict_kickstart():
-    return '1'
+
+def predict_kickstart(goal, duration, cat, country):
+    data = np.zeros(20)
+    data[0] = goal
+    data[1] = duration
+    data[int(cat)] = 1
+    data[int(country)] = 1
+    return model.predict_proba([data])
